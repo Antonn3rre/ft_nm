ba4e7a3ad80fa223121c721a67c765e617ad1329
agozlan <antongozlan@hotmail.com>
2025-09-30 14:58:29 +0200

    Debut printInfo type

--- printInfo.c
+++ printInfo.c
@@ -1,6 +1,38 @@
 #include "ft_nm.h"
 #include <elf.h>
 
+void printType64(t_64bits info, Elf64_Sym *actual) {
+	// Ordre de check : U, u, A, B/b, D/d, C/c, G/g, I,
+	// N, n, P, R/r, S/s, T/t, -, V/v, W/w, si rien ?
+
+	char c = '?';
+
+	if (actual->st_shndx == SHN_UNDEF)
+		c = 'U';
+	else if (ELF64_ST_BIND(actual->st_info) == STB_GNU_UNIQUE)
+		c = 'u';
+	else if (actual->st_shndx == SHN_ABS)
+		c = 'A';
+	else if (info.section[actual->st_shndx].sh_type == SHT_NOBITS &&
+	         (info.section[actual->st_shndx].sh_flags &
+	          (SHF_ALLOC | SHF_WRITE)) == (SHF_ALLOC | SHF_WRITE)) {
+		c = 'B';
+		if (ELF64_ST_BIND(actual->st_info) == STB_LOCAL)
+			c = 'b';
+	} else if (info.section[actual->st_shndx].sh_type == SHT_PROGBITS &&
+	           (info.section[actual->st_shndx].sh_flags &
+	            (SHF_ALLOC | SHF_WRITE)) == (SHF_ALLOC | SHF_WRITE)) {
+		c = 'D';
+		if (ELF64_ST_BIND(actual->st_info) == STB_LOCAL)
+			c = 'd';
+	} else if (ELF64_ST_TYPE(actual->st_info) == STT_COMMON &&
+	           info.section[actual->st_shndx].sh_type == SHN_COMMON) {
+		c = 'C';
+		if (ELF64_ST_BIND(actual->st_info) == STB_LOCAL)
+			c = 'c';
+	}
+	printf(" %c ", c);
+}
 int printInfo(t_nm *info) {
 
 	if (info->is64Bits) {
@@ -17,18 +49,18 @@ int printInfo(t_nm *info) {
 		     i++) {
 
 			Elf64_Sym *actual = &sectionElements[i];
-			printf("Index name = %u, info = %c, value = %lu\n", actual->st_name,
-			       actual->st_info, actual->st_value);
-			if (actual->st_info == STT_OBJECT)
-				printf("object\n");
-			else if (actual->st_info == STT_FUNC)
-				printf("function\n");
+			//printf("Index name = %u, info = %c, value = %lu\n", actual->st_name,
+			//       actual->st_info, actual->st_value);
 
 			char *name = (char *)info64.header + info64.strtab->sh_offset +
 			             actual->st_name;
 			//			printf("Name = %s\n", name);
-
-			printf("%016lu %c %s\n", actual->st_value, actual->st_info, name);
+			if (!actual->st_value)
+				printf("                ");
+			else
+				printf("%016lu", actual->st_value);
+			printType64(info64, actual);
+			printf("%s\n", name);
 		}
 	}
 
